5d901dd204edb2246c67222afc148910a2f2f167
Kevin <kevikoltraka157@gmail.com>
2025-04-13 18:39:19 +0200

    changes

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -9,14 +9,14 @@ export function ModeToggle() {
   const { theme, setTheme } = useTheme();
 
   React.useEffect(() => {
-    setTheme('light'); // Force light theme on initial load
+    setTheme('dark'); 
   }, [setTheme]);
 
   return (
     <Button
       variant="ghost"
       size="icon"
-      onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
     >
       <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
       <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
